ce000a60449f6a78311b7be1b2f8fe37e7a94f79
Marwan Mohamed <mmzidane101@gmail.com>
2025-10-12 16:05:52 +0000

    fact_table

--- dbt/taxi_rides_ny/models/core/fact_trips.sql
+++ dbt/taxi_rides_ny/models/core/fact_trips.sql
@@ -1,36 +1,22 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
+{{ config(materialized="table") }}
 
-with november as (
-    select *,
-    'November' as fiscal_month
-    from {{ ref('stg_2024_11') }}
-),
-december as (
-    select *,
-    'December' as fiscal_month
-    from {{ ref('stg_2024_12') }}
-),
+with
+    november as (select *, 'November' as fiscal_month from {{ ref("stg_2024_11") }}),
+    december as (select *, 'December' as fiscal_month from {{ ref("stg_2024_12") }}),
 
-trips_unioned as (
-    select *
-    from december
-    union all
-     select *
-    from november
-),
+    trips_unioned as (
+        select *
+        from december
+        union all
+        select *
+        from november
+    ),
 
-dim_zones as (
-    select * from {{ ref('dim_zones') }}
-    where feed_borough != "Unknown"
-)
+    dim_zones as (select * from {{ ref("dim_zones") }} where feed_borough != "Unknown")
 
-select * 
+select *
 from trips_unioned
-inner join dim_zones as pickup_zone
-on trips_unioned.PULocationID = pickup_zone.locationid
-inner join dim_zones as dropoff_zone
-on trips_unioned.DOLocationID = pickup_zone.locationid
+inner join
+    dim_zones as pickup_zone on trips_unioned.pulocationid = pickup_zone.feed_loc_id
+inner join
+    dim_zones as dropoff_zone on trips_unioned.dolocationid = dropoff_zone.feed_loc_id
