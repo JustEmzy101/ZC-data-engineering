7f5617cdcb4142ebc3831c5593164a24b28bfec6
Marwan Mohamed <mmzidane101@gmail.com>
2025-10-15 08:28:29 +0000

    4.3M rows

--- dbt/taxi_rides_ny/models/staging/stg_green_trips.sql
+++ dbt/taxi_rides_ny/models/staging/stg_green_trips.sql
@@ -41,12 +41,12 @@ select
     coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type,
     {{ get_payment_type_description("payment_type") }} as payment_type_description
 from tripdata
-where rn = 1
+
 
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
-{% if var('is_test_run', default=true) %}
+-- {% if var('is_test_run', default=true) %}
 
-  limit 100
+--   limit 100
 
-{% endif %}
+-- {% endif %}
